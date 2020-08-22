e5a24cb1a22a8161abb39de3c9d8a20f3e4089a7
Sam Mudick <smudick@gmail.com>
2020-08-21 21:37:11 -0500

    set consistent sizing for images

--- javascripts/river.js
+++ javascripts/river.js
@@ -3,9 +3,9 @@ const bearCard = (array) => {
   array.forEach((item) => {
     $("#card-container").append(`
         <div class="card m-3 bear-card" style="width: 18rem;">
-      <img src="${item.bearImage}" class="card-img-top" alt="photo of bear named ${item.bearName}">
+      <img src="${item.bearImage}" class="card-img-top bear-pic" alt="photo of bear named ${item.bearName}">
       <div class="card-body">
-        <h5 class="card-title text-center bear-name">${item.bearName}</h5>
+        <h5 class="card-title text-center">${item.bearName}</h5>
       </div>
     </div>`);
   });
